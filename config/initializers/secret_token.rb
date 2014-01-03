46d8087e34a0accb3222ebf1312df5e9afbdac0a
ahmedhawas <ahmedhawas7@gmail.com>
2014-01-03 16:11:22 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sizzle::Application.config.secret_key_base = 'ea0cb3c5b2e3fa872e2ac9cf441fc99c2214869af9731807a95d880950d333f5daffe9e06d6e3059d8d8ec1f2df32a375665fbde651115456c1200b3388e89e4'
